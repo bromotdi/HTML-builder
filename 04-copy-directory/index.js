caace64574a499cd4cff924f18be374027241b4b
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2023-05-09 01:28:27 +0300

    fix: correct path for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const fs = require("fs"), path = require("path");
+const dirNameCopyFiles = "files", dirNamePasteFiles = "files-copy";
+const pathCopyFiles = path.resolve(__dirname, dirNameCopyFiles);
+const pathPasteFiles = path.resolve(__dirname, dirNamePasteFiles);
 
+fs.readdir(pathCopyFiles, (error, files) => copyDir(files));
+
+function copyDir(files) {
+    fs.stat(pathPasteFiles, (error) => {
+        if (error) fs.mkdir(pathPasteFiles, (error) => {
+            clearDir(files, function () { copy(files); })
+        });
+        else clearDir(files, function () { copy(files); });
+    });
+}
+
+function clearDir(files, callback) {
+    fs.readdir(pathPasteFiles, (error, files) => {
+        for (const file of files) {
+            fs.unlink(path.join(pathPasteFiles, file), (err) => {
+                if (err) throw err;
+            });
+        }
+        callback(files);
+    });
+}
+
+function copy(files) {
+    for (const file of files) {
+        const filePath = path.join(pathCopyFiles, file);
+        const filePathToPaste = path.join(pathPasteFiles, file);
+        fs.copyFile(filePath, filePathToPaste, (error) => {});
+    }
+}
