e245c270df368c97a2e70ee718505ac1746b7317
Daria Bromot <80320446+bromotdi@users.noreply.github.com>
2023-05-09 01:26:15 +0300

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+const process = require('process')
+const { stdout, stdin } = require('process')
+
+const textFile = fs.createWriteStream(path.join(__dirname, 'text.txt'))
+
+stdout.write('Write the text...\n')
+stdin.on('data', (data) => {
+    if (data.toString().trim() === 'exit') process.exit();
+    textFile.write(data, (error) => {
+        if (error) throw error;
+    })
+})
+process.on('SIGINT', () => process.exit());
+process.on('exit', (code) => {
+    if (code === 0) stdout.write('Goodbye');
+});
